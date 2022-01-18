1bc86666d3d11814e82752ce3f7b01bebd379140
yura.mail.ru@inbox.ru <yura.mail.ru@inbox.ru>
2022-01-18 19:41:54 +0400

    aaa

---
+++ script.js
@@ -0,0 +1,89 @@
+// let i = 0;
+// while (i < 10) {
+//     i++;
+//     console.log(i);
+// }
+
+// let i = 1,
+//     fact = 1,
+//     num = 5;
+
+// while(i <= num) {
+//     fact = fact * i;
+//     i++;
+// }    
+
+// console.log(fact);
+
+
+
+// for(let i = 0; i <= 10; i++) {
+//     if ( i % 2)
+// }
+
+
+
+
+// const person = new Object();
+
+// person.fistName = 'Davit';
+// person.lastName = 'Sargsyan';
+// person.age = 22;
+
+// console.log(person);
+
+
+
+
+// const person = {
+//     firstName: 'Davit',
+//     lastNAme: 'Sargsyan',
+//     age: 22
+// }
+
+// console.log(person, 'person');
+
+
+
+
+// const language = 'Russian';
+
+// let message;
+// if(language === 'English') {
+//     message = 'Hello'
+// } else if (language === 'Russian') {
+//     message = 'Privet'
+// } else if (language === 'Ispan') {
+//     message = 'Ola'
+// }
+
+
+
+
+
+// let length = 0; 
+// const languagesData = {
+//         English: 'Hello',
+//         Ispan: 'Ola'
+// }
+
+// for(let i in languagesData) {
+//     length++;
+//     languagesData.length = length
+// }
+
+
+
+const languagesData1 = {
+    English: 'Hello',
+    Ispan: 'Ola',
+    x: 10,
+    y: 20
+}
+
+const languagesData2 = {...languagesData1};
+
+
+
+console.log(languagesData1, 'languagesData1');
+console.log(languagesData2, 'languagesData2');
